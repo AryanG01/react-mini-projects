5751dc91b10a1873064e919190da26841566a6ed
AryanG01 <rajeevganju0@gmail.com>
2024-07-19 16:55:23 +0800

    Day 1

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+//Class based components 
+// import {Component} from 'react';
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hi, my name is Aryan</h1>
+//         <div>
+//           <h2>My favorite foods:</h2>
+//           <ol>
+//             <li>Chicken Biryani</li>
+//             <li>Chicken Shawarma</li>
+//             <li>Chicken Burger</li>
+//           </ol>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+
+// Function components are the simplest way to define a component in React.
+// function App() {
+//   return <section>
+//     <h1>Hi, my name is Aryan</h1>
+//     <div>
+//       <h2>My favorite foods:</h2>
+//       <ol>
+//         <li>Chicken Biryani</li>
+//         <li>Chicken Shawarma</li>
+//         <li>Chicken Burger</li>
+//       </ol>
+//     </div>
+//   </section>
+// }
+
+// export default App;
+
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return _jsxs("section", {
+    children: [_jsx("h1", {
+      children: "Hi, my name is Aryan"
+    }), _jsxs("div", {
+      children: [_jsx("h2", {
+        children: "My favorite foods:"
+      }), _jsxs("ol", {
+        children: [_jsx("li", {
+          children: "Chicken Biryani"
+        }), _jsx("li", {
+          children: "Chicken Shawarma"
+        }), _jsx("li", {
+          children: "Chicken Burger"
+        })]
+      })]
+    })]
+  });
 }
-
 export default App;
